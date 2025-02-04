3c160c5b7ab5fbda7c857778f649f70c2e2bb421
flotr18 <trouilletflorent@outlook.com>
2025-02-04 10:09:03 +0100

    Improved BestNumber (Use of Array)

--- BestNumber.py
+++ BestNumber.py
@@ -4,15 +4,14 @@ def BestNumber():
     x = random.randint(1,100)
     y = random.randint(1,100)
     z = random.randint(1,100)
+    
+    MyArray = [x, y, z]
+    
 
     print(f"x = {x}, y = {y}, z = {z}")
-
-    if x > y and x > z:
-        print("x is the best number")
-    elif y > x and y > z:
-        print("y is the best number")
-    else:    
-        print("z is the best number")
+    
+    largest = max(MyArray)
+    print(f"The largest number is {largest}")
 
     
 
